47e1f07528bf25f8490467325e15d89895adb997
Yodadude2003 <yodadude2003@gmail.com>
2022-04-17 17:29:44 -0500

    change smi timings properly again again

--- rpi_driver.c
+++ rpi_driver.c
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, terminate);
     map_devices();
-    init_smi(0, 20, 6, 13, 6);
+    init_smi(0, 50, 6, 13, 6);
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
     smi_cs->clear = 1;
