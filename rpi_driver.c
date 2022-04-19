3d99e7d7aeab9d0e6c34f861a044cffe58e1118c
Yodadude2003 <yodadude2003@gmail.com>
2022-04-19 17:29:19 -0500

    :(((

--- rpi_driver.c
+++ rpi_driver.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, terminate);
     map_devices();
-    init_smi(0, 10, 6, 13, 6);
+    init_smi(0, 9, 6, 13, 6);
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
     smi_cs->clear = 1;
