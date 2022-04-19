3d43bfda6315a83cdc3fbeca307b4cc54e648213
Yodadude2003 <yodadude2003@gmail.com>
2022-04-19 11:24:02 -0500

    another lil' fix

--- rpi_driver.c
+++ rpi_driver.c
@@ -36,7 +36,7 @@
 #define DAC_D0_PIN      8
 #define DAC_NPINS       8
 
-#define NSAMPLES        635
+#define NSAMPLES        636
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
