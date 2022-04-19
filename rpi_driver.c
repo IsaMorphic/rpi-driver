1df4426dd71bb992c157115df10b63d61f50dfc6
Yodadude2003 <yodadude2003@gmail.com>
2022-04-19 17:47:58 -0500

    trying sample rate increase again cause I was domb

--- rpi_driver.c
+++ rpi_driver.c
@@ -37,7 +37,7 @@
 #define DAC_D0_PIN      8
 #define DAC_NPINS       8
 
-#define NSAMPLES        636
+#define NSAMPLES        635 * 2
 #define NBUFFERS        262
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, terminate);
     map_devices();
-    init_smi(0, 100, 25, 50, 25);
+    init_smi(0, 9, 6, 13, 6);
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
     smi_cs->clear = 1;
