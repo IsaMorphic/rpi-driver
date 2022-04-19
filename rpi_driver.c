e950f4bdbbe4dd93b84127b239fb74be759d80cd
Yodadude2003 <yodadude2003@gmail.com>
2022-04-19 16:51:30 -0500

    that was too many buffers :(

--- rpi_driver.c
+++ rpi_driver.c
@@ -38,7 +38,7 @@
 #define DAC_NPINS       8
 
 #define NSAMPLES        9540
-#define NBUFFERS        32
+#define NBUFFERS        16
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
