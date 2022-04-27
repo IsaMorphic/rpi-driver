3054d1b404ff006f74f339681c9b533314d66d03
Yodadude2003 <yodadude2003@gmail.com>
2022-04-26 19:18:56 -0500

    last one for the night

--- rpi_driver.c
+++ rpi_driver.c
@@ -185,7 +185,7 @@ int main(int argc, char *argv[])
             if (time_difference < 0)
                 time_difference += 1000000000;
 
-            if(time_difference % (636 * 525 * 100) < 5000)
+            if(time_difference < (636 * 525 * 100))
                 break;
         }
     }
