634a2c04c2b9a474d70b0939876b2d6eda4009d3
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-11-23 16:56:41 -0600

    idfk lol

--- rpi_driver.c
+++ rpi_driver.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 50, 25, 50, 25);
+    init_smi(0, 100, 25, 25, 50);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
