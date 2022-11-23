ffde3096f4cdbe042e0027fbc884931814d9b59b
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-11-23 17:12:18 -0600

    fuck this lmao

--- rpi_driver.c
+++ rpi_driver.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 100, 20, 50, 20);
+    init_smi(0, 2,  6, 13,  6);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
