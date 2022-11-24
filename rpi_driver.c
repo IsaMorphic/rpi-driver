eed670bc286b27637dc3e9189f7c5587db2dd9b3
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-11-24 09:59:52 -0600

    fix clock speed lol

--- rpi_driver.c
+++ rpi_driver.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 2,  6, 13,  6);
+    init_smi(0, 4, 6, 13, 6);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
