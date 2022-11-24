9cc03675aedf512816fd9c64abe76fb2a21ab3cd
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-11-24 10:45:29 -0600

    That worked really well! Revert this commit if this doesn't work better

--- rpi_driver.c
+++ rpi_driver.c
@@ -164,7 +164,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 4, 6, 13, 6);
+    init_smi(0, 10, 3, 4, 3);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
