be8d88ae4a6680c5ac9d571ec983d01786a740ba
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-12-28 15:33:26 -0600

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
         signal(SIGINT, terminate);
 
         map_devices();
-        init_smi(0, 8,  6, 13,  6);
+        init_smi(0, 4,  6, 13,  6);
 
         gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
         gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
@@ -220,7 +220,7 @@ void dac_init(void)
         map_uncached_mem(&vc_mem[i], VC_MEM_SIZE(NSAMPLES));
 
     smi_dsr->rwidth = SMI_8_BITS;
-    smi_l->len = NSAMPLES * (NBUFFERS + 10) * TX_SAMPLE_SIZE;
+    smi_l->len = NSAMPLES * NBUFFERS * TX_SAMPLE_SIZE;
     smi_dmc->dmaen = 1;
     smi_cs->clear = 1;
     smi_cs->write = 1;
