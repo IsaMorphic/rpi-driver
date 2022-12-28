4cae0a16460828ec0d8d372c698c900e172ad5b3
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-12-28 14:57:22 -0600

    OKAY THIS SHOULD FIX IT!!!!!!!!~~`````1111111

--- rpi_driver.c
+++ rpi_driver.c
@@ -37,7 +37,7 @@
 #define DAC_D0_PIN      8
 #define DAC_NPINS       8
 
-#define NSAMPLES        858
+#define NSAMPLES        636
 #define NBUFFERS        525
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
