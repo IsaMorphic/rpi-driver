e0075c525a225ba78e7f0e1d65b3417bf9f3dedd
Isabelle Santin <isabelle@chosenfewsoftware.com>
2022-12-28 18:20:27 -0600

    double

--- rpi_driver.c
+++ rpi_driver.c
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
         signal(SIGINT, terminate);
 
         map_devices();
-        init_smi(0, 10, 3, 4, 3);
+        init_smi(0, 20, 3, 4, 3);
 
         gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
         gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
