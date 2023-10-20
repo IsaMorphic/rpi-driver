cfd225b3feec5856cd9c4f65f7a918da09411184
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-20 17:59:52 -0500

    to sleep or to microsleep, that is the question

--- rpi_driver.c
+++ rpi_driver.c
@@ -172,7 +172,7 @@ int main(int argc, char *argv[])
     do
     {
         dac_start();
-        usleep(50000);
+        sleep(50);
         read_count = dac_next(file_ptr);
     } while(read_count > 0 && !feof(file_ptr));
 
