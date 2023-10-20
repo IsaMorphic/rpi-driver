a72d4b8db3af69f1dc24e6c2f036f19eed9b1d78
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-20 11:25:59 -0500

    oopsie (delay funni)

--- rpi_driver.c
+++ rpi_driver.c
@@ -186,7 +186,7 @@ int main(int argc, char *argv[])
             do
             {
                 dac_start();
-                usleep(100);
+                usleep(100100);
                 read_count = dac_next(file_ptr);
             } while(read_count > 0 && !feof(file_ptr));
 
