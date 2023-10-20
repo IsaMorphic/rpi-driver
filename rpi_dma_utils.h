1c157ada7cdffe3a305aa735b4a5ed9b621329f3
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-20 13:41:24 -0500

    test

--- rpi_dma_utils.h
+++ rpi_dma_utils.h
@@ -19,7 +19,7 @@
 #define PHYS_REG_BASE   PI_4_REG_BASE
 #define PI_01_REG_BASE  0x20000000  // Pi Zero or 1
 #define PI_23_REG_BASE  0x3F000000  // Pi 2 or 3
-#define PI_4_REG_BASE   0xFE000000  // Pi 4
+#define PI_4_REG_BASE   0x47E000000  // Pi 4
 
 #define CLOCK_HZ      250000000   // Pi 2 - 4
 //#define CLOCK_HZ        400000000   // Pi Zero
