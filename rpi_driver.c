e3396d58601171fca7ce74238e8aff1191eaf021
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-21 12:43:59 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -38,7 +38,7 @@
 #define DAC_NPINS       8
 
 #define NSAMPLES        2002
-#define NBUFFERS        625
+#define NBUFFERS        125
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
