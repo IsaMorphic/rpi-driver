791d390f6c81803ed7d53939c7dbde6e0d37a28d
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-21 12:51:04 -0500

    maybe this

--- rpi_driver.c
+++ rpi_driver.c
@@ -172,7 +172,7 @@ int main(int argc, char *argv[])
     do
     {
         dac_start();
-        usleep(150);
+        usleep(175);
         read_count = dac_next(file_ptr);
     } while(read_count > 0 && !feof(file_ptr));
 
