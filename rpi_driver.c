e75f9373cf80c23194e71bc2aa8a98147ebecb56
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 12:08:45 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -205,7 +205,7 @@ void dac_init(void)
         map_uncached_mem(&vc_mem[i], VC_MEM_SIZE(NSAMPLES));
 
     smi_dsr->rwidth = SMI_8_BITS;
-    smi_l->len = NSAMPLES * NBUFFERS * TX_SAMPLE_SIZE * 2;
+    smi_l->len = NSAMPLES * NBUFFERS * TX_SAMPLE_SIZE;
     smi_dmc->dmaen = 1;
     smi_cs->clear = 1;
     smi_cs->write = 1;
