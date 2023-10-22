32a3b8f8306e027ede3bc5b82dc7450f8c65f3bb
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 12:19:49 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -37,8 +37,8 @@
 #define DAC_D0_PIN      8
 #define DAC_NPINS       8
 
-#define NSAMPLES        1589
-#define NBUFFERS        525
+#define NSAMPLES        4004
+#define NBUFFERS        625
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
@@ -175,7 +175,7 @@ int main(int argc, char *argv[])
     clock_gettime(CLOCK_MONOTONIC, &deadline);
     do
     {
-        deadline.tv_nsec += 66700000;
+        deadline.tv_nsec += 200200000;
         if(deadline.tv_nsec >= 1000000000) 
         {  
             deadline.tv_nsec -= 1000000000;
