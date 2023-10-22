16c5ad5dada601e2f36b44b42e7b41d1148bc218
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 15:35:08 -0500

    wow that was like really cool

--- rpi_driver.c
+++ rpi_driver.c
@@ -179,7 +179,7 @@ int main(int argc, char *argv[])
         clock_gettime(CLOCK_MONOTONIC, &deadline);
         dac_next();
 
-        deadline.tv_nsec += 900900000;
+        deadline.tv_nsec += 300300000;
         if(deadline.tv_nsec >= 1000000000) 
         {  
             deadline.tv_nsec -= 1000000000;
@@ -210,7 +210,7 @@ void dac_init(void)
         map_uncached_mem(&vc_mem[i], VC_MEM_SIZE(NSAMPLES));
 
     smi_dsr->rwidth = SMI_8_BITS;
-    smi_l->len = NSAMPLES * NBUFFERS * 3 * TX_SAMPLE_SIZE;
+    smi_l->len = NSAMPLES * NBUFFERS * 2 * TX_SAMPLE_SIZE;
     smi_dmc->dmaen = 1;
     smi_cs->clear = 1;
     smi_cs->write = 1;
