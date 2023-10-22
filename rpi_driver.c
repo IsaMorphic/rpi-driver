4c04be6037b3168d6959e2c2306b7a507a8e55c3
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 12:10:07 -0500

    trying hard

--- rpi_driver.c
+++ rpi_driver.c
@@ -159,7 +159,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 4,  3,  8,  4);
+    init_smi(0, 8,  3,  8,  4);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
