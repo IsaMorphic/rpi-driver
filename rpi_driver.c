5e2703f6bc38b8915e12cc002c5fcca3dbafc5c6
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 12:44:22 -0500

    4 fps is good enough for now :(

--- rpi_driver.c
+++ rpi_driver.c
@@ -189,7 +189,7 @@ int main(int argc, char *argv[])
 
     dac_init();
 
-    int interval = 62500;
+    int interval = 250000;
     pthread_create(&thread, NULL, do_smth_periodically, &interval);
 
     for(;;)
