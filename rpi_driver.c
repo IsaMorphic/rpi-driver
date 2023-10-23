c664016b3cb7b334620776f9d3e47fe304648e50
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 11:24:32 -0500

    wow I think this might be it

--- rpi_driver.c
+++ rpi_driver.c
@@ -39,7 +39,7 @@
 
 #define NSAMPLES        795
 #define NBUFFERS        525
-#define NFRAMES         5
+#define NFRAMES         30
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
@@ -185,7 +185,7 @@ int main(int argc, char *argv[])
 
         for(frame_num = 0; frame_num < NFRAMES; frame_num++)
         {
-            deadline.tv_nsec += NSAMPLES * NBUFFERS * 78;
+            deadline.tv_nsec += NSAMPLES * NBUFFERS * 79;
             if(deadline.tv_nsec >= 1000000000) 
             {  
                 deadline.tv_nsec -= 1000000000;
