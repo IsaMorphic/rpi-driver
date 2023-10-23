a642250fce5cdffd17130dcc13b4a6f69e361ea1
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 20:42:56 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -172,11 +172,9 @@ int main(int argc, char *argv[])
     file_ptr = stdin;
 
     dac_init();
-    read_count = buff_next(file_ptr);
-
+    clock_gettime(CLOCK_MONOTONIC, &deadline);
     do
     {
-        clock_gettime(CLOCK_MONOTONIC, &deadline);
         deadline.tv_nsec += 603500000;
         if(deadline.tv_nsec >= 1000000000) 
         {  
@@ -184,11 +182,11 @@ int main(int argc, char *argv[])
             deadline.tv_sec++;
         }
 
-        dac_next();
-        dac_start();
-
         read_count = buff_next(file_ptr);
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL);
+        
+        dac_next();
+        dac_start();
     } while(read_count > 0 && !feof(file_ptr));
 
     terminate(0);
