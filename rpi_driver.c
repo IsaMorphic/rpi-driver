90e9e9eeaef07225f4dfe7c3e840ed8eda08b751
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 11:00:31 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -39,7 +39,7 @@
 
 #define NSAMPLES        7150
 #define NBUFFERS        525
-#define NFRAMES         1
+#define NFRAMES         3
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
 #define SMI_CS      0x00    // Control & status
@@ -183,7 +183,7 @@ int main(int argc, char *argv[])
 
         for(frame_num = 0; frame_num < NFRAMES; frame_num++)
         {
-            deadline.tv_nsec += NSAMPLES * NBUFFERS * 75;
+            deadline.tv_nsec += NSAMPLES * NBUFFERS * 78;
             if(deadline.tv_nsec >= 1000000000) 
             {  
                 deadline.tv_nsec -= 1000000000;
