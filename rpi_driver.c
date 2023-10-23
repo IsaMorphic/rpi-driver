e6f9f191a192d497dde2ce65bf5898f811dfc636
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-22 22:03:48 -0500

    1

--- rpi_driver.c
+++ rpi_driver.c
@@ -175,7 +175,7 @@ int main(int argc, char *argv[])
     do
     {
         clock_gettime(CLOCK_MONOTONIC, &deadline);
-        deadline.tv_nsec += 10195000;
+        deadline.tv_nsec += 10215000;
         if(deadline.tv_nsec >= 1000000000) 
         {  
             deadline.tv_nsec -= 1000000000;
