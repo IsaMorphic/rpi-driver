a242476da63d39c5571e5ced93dc48a598c9df6e
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 11:50:30 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -37,7 +37,7 @@
 #define DAC_D0_PIN      8
 #define DAC_NPINS       8
 
-#define NSAMPLES        22880
+#define NSAMPLES        795
 #define NBUFFERS        525
 
 #define SMI_BASE    (PHYS_REG_BASE + 0x600000)
@@ -187,7 +187,7 @@ int main(int argc, char *argv[])
             deadline.tv_nsec -= 1000000000;
             deadline.tv_sec++;
         }
-        
+
         read_count = buff_next(file_ptr);
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &deadline, NULL);
     }
