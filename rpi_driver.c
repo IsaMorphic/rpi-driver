a6967cb6ca3d7c5e1f25f62610475da50326a458
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 11:35:33 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -161,7 +161,7 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate);
 
     map_devices();
-    init_smi(0, 6, 5, 10, 5);
+    init_smi(0, 12, 5, 10, 5);
 
     gpio_mode(SMI_SOE_PIN, GPIO_ALT1);
     gpio_mode(SMI_SWE_PIN, GPIO_ALT1);
