6f8b186ba90a015f602d31c6c204256bf3964514
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-23 10:22:27 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -199,7 +199,7 @@ int main(int argc, char *argv[])
         
         for(frame_num = 0; frame_num < NFRAMES; frame_num++)
         {
-            deadline.tv_nsec += (NSAMPLES - parity_flag) * NBUFFERS * 80;
+            deadline.tv_nsec += NSAMPLES * NBUFFERS * 80;
             if(deadline.tv_nsec >= 1000000000) 
             {  
                 deadline.tv_nsec -= 1000000000;
