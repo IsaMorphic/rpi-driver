59e904bbf5f4d7df5856336f7021ec4e4117f0bc
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-24 16:08:29 -0500

    test

--- rpi_driver.c
+++ rpi_driver.c
@@ -173,7 +173,7 @@ int main(int argc, char *argv[])
             while(read_count > 0 && !feof(file_ptr))
             {
                 dac_start();
-                usleep(38180);
+                usleep(38195);
                 read_count = buff_next(file_ptr);
             }
 
