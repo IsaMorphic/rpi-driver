6f2930ce1190698169f2e3c2015bd694146f6d62
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-10-24 10:21:59 -0500

    still testing the sync delays

--- rpi_driver.c
+++ rpi_driver.c
@@ -174,7 +174,7 @@ int main(int argc, char *argv[])
             while(read_count > 0 && !feof(file_ptr))
             {
                 dac_start();
-                usleep(20750);
+                usleep(21000);
                 read_count = buff_next(file_ptr);
             }
 
